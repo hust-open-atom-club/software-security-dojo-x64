93aefb525aa72fda98866cc7b1118a6f41389d9a
Dongliang Mu <dzm91@hust.edu.cn>
2023-08-11 16:14:41 +0800

    revise welcome message

--- simple-demo/level-1-1/simple-demo-level1.1.c
+++ simple-demo/level-1-1/simple-demo-level1.1.c
@@ -5,7 +5,7 @@
 void print_desc()
 {
 	printf("###\n");
-	printf("### Welcome to ./demo_level1.0!\n");
+	printf("### Welcome to ./simple-demo-level1.0!\n");
 	printf("###\n");
 	printf("\n");
 	printf("This challenge will directly read /flag and print it out.\n");
