a34f73e91d0285466be954676613678b6a2e507f
Dongliang Mu <dzm91@hust.edu.cn>
2023-08-31 19:33:14 +0800

    remove Chinese font in the challenge

--- elf-crackme/level-1-0/utility.py
+++ elf-crackme/level-1-0/utility.py
@@ -1,23 +1,25 @@
+#!/opt/pwn.college/python
+
 def main():
     file_path = "./elf-crackme-level1.0"
     
     try:
         with open(file_path, "r+b") as file:
-            position = int(input("请输入要修改的位置（16进制），例如：0x1000："), 16)
-            new_data = int(input("请输入新的 byte 数据（16进制），例如：01："), 16)
+            position = int(input("Please provide the offset of ELF file in hex format, e.g., 0x1000"), 16)
+            new_data = int(input("Please provide the data byte by byte in hex format, e.g., 01"), 16)
             
             file.seek(position)
             file.write(bytes([new_data]))
             
-        print("修改完成！")
+        print("Done!")
     except FileNotFoundError:
-        print("无法打开文件")
+        print("[-] open failed")
     except Exception as e:
-        print("发生错误:", e)
+        print("Error occurs", e)
 
 if __name__ == "__main__":
-    print("你能看出x86和x64两种的ELF-header的区别吗？")
-    print("我们提供了一个x64的文件，你能修复他吗？修复完成后执行即可获得flag。")
+    print("Can you identify the differences of ELF header between x86 and x86_64?")
+    print("We provide a x86_64 ELF file. Can you please fix it and run it to get flag?")
     while True:
         main()
 
