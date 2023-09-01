c1299666cb8a0f10cadac2b08feddb6a850461c7
Wum1ng <Wum1ng@Wum1ng-MacBook-Pro.local>
2023-09-01 17:55:08 +0800

    Update elf-crackme  glibc version.

--- elf-crackme/level-1-0/utility.py
+++ elf-crackme/level-1-0/utility.py
@@ -5,11 +5,11 @@ def main():
     
     try:
         with open(file_path, "r+b") as file:
-            position = int(input("Please provide the offset of ELF file in hex format, e.g., 0x1000\n"), 16)
-            new_data = int(input("Please provide the data byte by byte in hex format, e.g., 01\n"), 16)
+            position = 0x4 
+            new_data = 0x1   # 将要写入的数据设置为0x66 0x66
             
             file.seek(position)
-            file.write(bytes([new_data]))
+            file.write(new_data.to_bytes(1, byteorder='little'))  # 使用little-endian字节顺序写入两个字节
             
         print("Done!")
     except FileNotFoundError:
@@ -18,8 +18,5 @@ def main():
         print("Error occurs", e)
 
 if __name__ == "__main__":
-    print("Can you identify the differences of ELF header between x86 and x86_64?")
-    print("We provide a x86_64 ELF file. Can you please fix it and run it to get flag?")
-    while True:
-        main()
+    main()
 
