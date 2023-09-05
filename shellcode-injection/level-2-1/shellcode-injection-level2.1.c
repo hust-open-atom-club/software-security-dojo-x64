3f553c4ddb0e1a803f27195a127e0218a2a0d097
Wum1ng <Wum1ng@Wum1ng-MBP.pride.lan>
2023-09-05 22:04:52 +0800

    update shellcode-injection.

---
+++ shellcode-injection/level-2-1/shellcode-injection-level2.1.c
@@ -0,0 +1,93 @@
+#include <sys/mman.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include <capstone/capstone.h>
+
+#define CAPSTONE_ARCH CS_ARCH_X86
+#define CAPSTONE_MODE CS_MODE_64
+
+void print_disassembly(void *shellcode_addr, size_t shellcode_size)
+{
+    csh handle;
+    cs_insn *insn;
+    size_t count;
+
+    if (cs_open(CAPSTONE_ARCH, CAPSTONE_MODE, &handle) != CS_ERR_OK)
+    {
+        printf("ERROR: disassembler failed to initialize.\n");
+        return;
+    }
+
+    count = cs_disasm(handle, shellcode_addr, shellcode_size, (uint64_t)shellcode_addr, 0, &insn);
+    if (count > 0)
+    {
+        size_t j;
+        printf("      Address      |                      Bytes                    |          Instructions\n");
+        printf("------------------------------------------------------------------------------------------\n");
+
+        for (j = 0; j < count; j++)
+        {
+            printf("0x%016lx | ", (unsigned long)insn[j].address);
+            for (int k = 0; k < insn[j].size; k++) printf("%02hhx ", insn[j].bytes[k]);
+            for (int k = insn[j].size; k < 15; k++) printf("   ");
+            printf(" | %s %s\n", insn[j].mnemonic, insn[j].op_str);
+        }
+
+        cs_free(insn, count);
+    }
+    else
+    {
+        printf("ERROR: Failed to disassemble shellcode! Bytes are:\n\n");
+        printf("      Address      |                      Bytes\n");
+        printf("--------------------------------------------------------------------\n");
+        for (unsigned int i = 0; i <= shellcode_size; i += 16)
+        {
+            printf("0x%016lx | ", (unsigned long)shellcode_addr+i);
+            for (int k = 0; k < 16; k++) printf("%02hhx ", ((uint8_t*)shellcode_addr)[i+k]);
+            printf("\n");
+        }
+    }
+
+    cs_close(&handle);
+}
+
+void *shellcode_mem;
+size_t shellcode_size;
+
+int main(int argc, char **argv, char **envp)
+{
+    assert(argc > 0);
+   
+    for (int i = 3; i < 10000; i++) close(i);
+    for (char **a = argv; *a != NULL; a++) memset(*a, 0, strlen(*a));
+    for (char **a = envp; *a != NULL; a++) memset(*a, 0, strlen(*a));
+
+    shellcode_mem = mmap((void *) 0x2d2d2000, 0x1000, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_ANON, 0, 0);
+    printf("[LEAK] Mapping shellcode memory at %p!\n", shellcode_mem);
+    assert(shellcode_mem == (void *) 0x2d2d2000);
+
+    puts("Reading 0x1000 bytes from stdin.\n");
+    shellcode_size = read(0, shellcode_mem, 0x1000);
+    assert(shellcode_size > 0);
+
+    puts("Executing filter...\n");
+    puts("This challenge requires that your shellcode have no H bytes!\n");
+    for (int i = 0; i < shellcode_size; i++)
+        if (((uint8_t*)shellcode_mem)[i] == 'H')
+        {
+            printf("Failed filter at byte %d!\n", i);
+            exit(1);
+        }
+
+    puts("This challenge is about to execute the following shellcode:\n");
+    print_disassembly(shellcode_mem, shellcode_size);
+    puts("");
+
+    puts("Executing shellcode!\n");
+    ((void(*)())shellcode_mem)();
+}
---
+++ shellcode-injection/level-3-0/shellcode-injection-level3.0.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    char buf[0x400];
+    int n, i;
+    n = read(0, buf, 0x400);
+    if (n <= 0) return 0;
+    for (i = 0; i < n; i++) {
+        if(buf[i] < 32 || buf[i] > 126) return 0;
+    }
+    ((void(*)(void))buf)();
+}
