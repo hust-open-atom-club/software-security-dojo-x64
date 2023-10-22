441788da744e980138ce991020a46ffd1b185cbf
GodGun123 <148665938+GodGun123@users.noreply.github.com>
2023-10-22 11:23:07 +0800

    adjust format

--- use-after-free/level-1-0/use-after-free-level1.0.c
+++ use-after-free/level-1-0/use-after-free-level1.0.c
@@ -17,19 +17,19 @@ void init()
 
 int read_int()
 {
-    char buf[0x10];
-    char *endptr;
-    long long val;
-    read(0, buf, sizeof(buf) - 1);
-    buf[sizeof(buf) - 1] = '\0';
-    val = strtoll(buf, &endptr, 10);
-    if (endptr == buf) {
-        exit(-1);
-    }
-    if (val < INT_MIN || val > INT_MAX) {
-        exit(-1);
-    }
-    return (int)val;
+    	char buf[0x10];
+    	char *endptr;
+    	long long val;
+    	read(0, buf, sizeof(buf) - 1);
+    	buf[sizeof(buf) - 1] = '\0';
+    	val = strtoll(buf, &endptr, 10);
+    	if (endptr == buf) {
+        	exit(-1);
+    	}
+    	if (val < INT_MIN || val > INT_MAX) {
+        	exit(-1);
+    	}
+    	return (int)val;
 }
 
 void read_flag()
